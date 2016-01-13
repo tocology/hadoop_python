649cf472b4b4915fdc753c71f1c7349a54a50a7e
JunHo Hwang <hwangjun7777@gmail.com>
2016-01-13 07:27:31 +0000

    app(feat): Add Most Popular Movie with Items program using ml-100k data

---
+++ app/MostPopularMovieNicer.py
@@ -0,0 +1,37 @@
+from mrjob.job import MRJob
+from mrjob.step import MRStep
+
+class MostPopularMovie(MRJob):
+    def configure_options(self):
+        super(MostPopularMovie, self).configure_options()
+        self.add_file_option('--items', help='Path to u.item')
+    
+    def steps(self):
+        return [
+            MRStep(mapper=self.mapper_get_ratings,
+                   reducer_init=self.reducer_init,
+                   reducer=self.reducer_count_ratings),
+            MRStep(reducer=self.reducer_find_max)
+        ]
+        
+    def mapper_get_ratings(self, _, line):
+        (userID, movieID, rating, timestamp) = line.split('\t')
+        yield movieID, 1
+        
+    def reducer_init(self):
+        self.movieNames = {} # dic = {movieID, movieName}
+        
+        with open("u.item") as f:
+            for line in f:
+                fields = line.split('|')
+                self.movieNames[fields[0]] = fields[1]
+        
+    def reducer_count_ratings(self, key, values):
+        yield None, (sum(values), self.movieNames[key])
+        
+    def reducer_find_max(self, key, values):
+        yield max(values)
+        
+        
+if __name__ == '__main__':
+    MostPopularMovie.run()
